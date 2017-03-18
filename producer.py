01dee58fb6d280ddb20badcbf5e98af3e5b94f4a
Matthew D Jones <matthew.d.jones@stfc.ac.uk>
2017-03-18 13:36:36 +0000

    Use correct IP for kafka broker

--- producer.py
+++ producer.py
@@ -1,7 +1,7 @@
 from kafka import KafkaProducer
 from streaming import avroserialise
 
-producer = KafkaProducer(bootstrap_servers=['localhost'])
+producer = KafkaProducer(bootstrap_servers=['192.168.252.82'])
 serialiser = avroserialise.AvroSerialise()
 for n in range(100):
     buffer = serialiser.serialise(message="message number " + str(n))
