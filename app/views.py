b9fd2d5973f6a0c4983029e0f891d907aa982be7
Matthew D Jones <matthew.d.jones@stfc.ac.uk>
2017-03-18 18:08:32 +0000

    Producing messages works

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import Flask, session, redirect, url_for, escape, request, Response, render_template
 from kafka import KafkaConsumer
+from kafka import KafkaProducer
+from app.streaming.avroserialiser import AvroSerialiser
 from app.streaming.avrodeserialiser import AvroDeserialiser
 from time import sleep
 
@@ -11,13 +13,15 @@ def index():
     return render_template("index.html")
 
 
+KAFKA_BROKER = '192.168.252.82'
 consumer = KafkaConsumer('test_avro_topic',
                          group_id=None,
-                         bootstrap_servers=['192.168.252.82'],
+                         bootstrap_servers=[KAFKA_BROKER],
                          auto_offset_reset='earliest',
                          enable_auto_commit=False)
-
-all_messages = ['A', 'B', 'C']
+producer = KafkaProducer(bootstrap_servers=[KAFKA_BROKER])
+serialiser = AvroSerialiser()
+all_messages = []
 deserialiser = AvroDeserialiser()
 
 
@@ -31,6 +35,7 @@ def get_messages():
                 for p in partitions:
                     for response in partitions[p]:
                         message = deserialiser.deserialise(response.value)
+                        all_messages.append(message)
                         yield "data: %s\n\n" % message.get_html()
 
         return Response(script(), content_type='text/event-stream')
@@ -40,5 +45,6 @@ def get_messages():
 @app.route('/send', methods=['POST'])
 def send():
     print(request.form['message'])
-    all_messages.append(request.form['message'])
+    buffer = serialiser.serialise(raw_text=request.form['message'])
+    producer.send('test_avro_topic', buffer)
     return ""
