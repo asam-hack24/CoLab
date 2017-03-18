4c2c8c5a2b89aac2f2349a3512667032e25d504a
Matthew D Jones <matthew.d.jones@stfc.ac.uk>
2017-03-18 15:37:33 +0000

    Do text message deserialisation

--- app/streaming/avroserialise.py
+++ app/streaming/avroserialise.py
@@ -1,6 +1,9 @@
 from fastavro import reader, writer
 import io
 import uuid
+from datetime import datetime
+from app.messages.message import MessageType
+from app.messages.message_text import TextMessage
 
 
 class AvroSerialise:
@@ -11,24 +14,35 @@ class AvroSerialise:
             {'name': 'id', 'type': 'int'},
             {'name': 'author', 'type': 'string'},
             {'name': 'type', 'type': 'string'},
-            {'name': 'text', 'type': 'string'},
+            {'name': 'raw_text', 'type': 'string'},
             {'name': 'html', 'type': 'string'},
+            {'name': 'timestamp', 'type': 'int'}
         ],
     }
 
     def __init__(self):
         pass
 
-    def serialise(self, text=''):
+    def serialise(self, raw_text=''):
         """Generate a unique id for the message and serialise it"""
         buffer = io.BytesIO()
         writer(buffer, self.schema, [{'id': uuid.uuid4().int,
                                       'author': 'Bob',
                                       'type': 'text',
-                                      'text': text,
-                                      'html': text}])
+                                      'raw_text': raw_text,
+                                      'timestamp': datetime.now().timestamp(),
+                                      'html': raw_text}])
         return buffer.getvalue()
 
     def deserialise(self, buffer):
         output = reader(io.BytesIO(buffer), self.schema)
-        return output
+        new_message = None
+        for message in output:
+            if message['type'] == MessageType.TEXT:
+                new_message = TextMessage(message['author'], 'last_author',
+                                          datetime.fromtimestamp(message['timestamp']),
+                                          datetime.fromtimestamp(0),
+                                          message['raw_text'])
+            else:
+                raise ValueError('Unrecognised message type in AvroSerialise.deserialise')
+        return new_message
--- producer.py
+++ producer.py
@@ -5,5 +5,5 @@ from app.streaming import avroserialise
 producer = KafkaProducer(bootstrap_servers=['192.168.252.82'])
 serialiser = avroserialise.AvroSerialise()
 for n in range(100):
-    buffer = serialiser.serialise(text="message number " + str(n))
+    buffer = serialiser.serialise(raw_text="message number " + str(n))
     producer.send('test_avro_topic', buffer)
