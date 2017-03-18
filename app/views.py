e32d4547ec109ab24813e83b154c998f3e1e9c43
Matthew D Jones <matthew.d.jones@stfc.ac.uk>
2017-03-18 15:54:22 +0000

    pep 8 views.py

--- app/views.py
+++ app/views.py
@@ -8,21 +8,22 @@ def index():
     return render_template("index.html")
 
 
-messages = ['A','B','C']
+messages = ['A', 'B', 'C']
+
 
 @app.route('/get_messages')
 def get_messages():
     if request.headers.get('accept') == 'text/event-stream':
         def script():
-            #a lot of code goes here
+            # a lot of code goes here
             print(messages)
             while messages:
-                yield "data: %s\n\n"%messages.pop(0)
+                yield "data: %s\n\n" % messages.pop(0)
 
         return Response(script(), content_type='text/event-stream')
     return redirect(url_for('static', filename='index.html'))
-    
-    
+
+
 @app.route('/send', methods=['POST'])
 def send():
     print(request.form['message'])
