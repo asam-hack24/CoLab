da6180cd866b44b8bc82c7df81d27d130d401e65
Asaf Paris-Mandoki <asafpm@gmail.com>
2017-03-18 15:52:45 +0000

    Added proof of concept

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import Flask, session, redirect, url_for, escape, request, Response, render_template
 
 
 @app.route('/')
@@ -8,3 +8,23 @@ def index():
     return render_template("index.html")
 
 
+messages = ['A','B','C']
+
+@app.route('/get_messages')
+def get_messages():
+    if request.headers.get('accept') == 'text/event-stream':
+        def script():
+            #a lot of code goes here
+            print(messages)
+            while messages:
+                yield "data: %s\n\n"%messages.pop(0)
+
+        return Response(script(), content_type='text/event-stream')
+    return redirect(url_for('static', filename='index.html'))
+    
+    
+@app.route('/send', methods=['POST'])
+def send():
+    print(request.form['message'])
+    messages.append(request.form['message'])
+    return ""
